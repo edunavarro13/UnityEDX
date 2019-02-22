8ea56cb094aff9a1f4d82dccd69edba3702311ea
Eduardo Navarro Poveda <edunavarro13@gmail.com>
2019-02-22 13:35:43 +0100

    Terminado Tema 3 del curso EDX

---
+++ New Unity Project/Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    // Version 1.0 -> en la que la bola se mueve, no se empuja con rigidbody
+    //public float speed;
+    public float forceValue;
+    public float jumpValue;
+    private Rigidbody rigidbody;
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Version 1.0 -> en la que la bola se mueve, no se empuja con rigidbody
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 
+        //    0,
+        //    Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        // Input.GetButton Se ha de mantener pulsado para dar true
+        // Input.GetButtonDown se ha de pulsar para dar true
+        // Jump es espacio en el ordenador
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.01f)
+        {
+            rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+            audio.Play();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical")) * forceValue);
+    }
+}
